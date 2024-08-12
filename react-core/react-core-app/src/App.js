372cdba808d43481afb45331965802c3119313d9
swifttaylo <mytaylorship@gmail.com>
2024-08-12 22:17:42 +0530

    React custom types

---
+++ react-core/react-core-app/src/App.js
@@ -0,0 +1,31 @@
+
+export function App() {
+  return (
+    <h1>React Core started!</h1>
+  );
+}
+
+
+const user = {
+  name: 'Hedy Lamarr',
+  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+  imageSize: 90,
+};
+
+export default function Profile() {
+  return (
+    <>
+      <h1>{user.name}</h1>
+      <img
+        className="avatar"
+        src={user.imageUrl}
+        alt={'Photo of ' + user.name}
+        style={{
+          width: user.imageSize,
+          height: user.imageSize
+        }}
+      />
+    </>
+  );
+}
+
